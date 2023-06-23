f153ab7e9f3bb6843efd54e62dec87e992c46012
yisy0713 <yisy0713@naver.com>
2023-06-24 03:07:06 +0900

    외부-내부 맵 구현

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -7,13 +7,18 @@ public class PlayerAction : MonoBehaviour
     public float Speed;
 
     Rigidbody2D rigid;
+    Animator anim;
     float h;
     float v;
     bool isHorizonMove;
+    Vector3 dirVec;
+    GameObject scanObject;
+
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -36,10 +41,62 @@ public class PlayerAction : MonoBehaviour
         bool vUp = Input.GetButtonUp("Vertical");
 
         // Check Horizontal Move
-        if (hDown || vUp)
+        if (hDown)
             isHorizonMove = true;
-        else if (vDown || hUp)
+        else if (vDown)
             isHorizonMove = false;
+        else if (hUp || vUp)
+            isHorizonMove = h != 0;
+
+        // Animation
+        if (anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if (anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+            anim.SetBool("isChange", false);
+
+        // Direction
+        if (vDown && v == 1)
+        {
+            dirVec = Vector3.up;
+        }
+        else if (vDown && v == -1)
+        {
+            dirVec = Vector3.down;
+        }
+        else if (hDown && h == -1)
+        {
+            dirVec = Vector3.left;
+        }
+        else if (hDown && h == 1)
+        {
+            dirVec = Vector3.right;
+        }
+
+        // Scan Object
+        if (Input.GetKey(KeyCode.E) && scanObject != null && scanObject.CompareTag("GoToOutDoor"))
+        {
+            //Debug.Log("This is :" + scanObject.name);
+            this.transform.position = new Vector3(0, -46, 0);
+            //manager.Action(scanObject);
+        }
+
+        if (Input.GetKey(KeyCode.E) && scanObject != null && scanObject.CompareTag("GoToInsideDoor"))
+        {
+            //Debug.Log("This is :" + scanObject.name);
+            this.transform.position = new Vector3(-6, -9, 0);
+            //manager.Action(scanObject);
+
+
+        }
+
     }
 
     void FixedUpdate()
@@ -47,5 +104,16 @@ public class PlayerAction : MonoBehaviour
         // Move
         Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
         rigid.velocity = moveVec * Speed;
+
+        // Ray
+        Debug.DrawRay(rigid.position, dirVec * 1.7f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 1.7f, LayerMask.GetMask("Object"));
+
+        if (rayHit.collider != null)
+        {
+            scanObject = rayHit.collider.gameObject;
+        }
+        else
+            scanObject = null;
     }
 }
